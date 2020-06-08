f1524dec62c7c22d20d6e89a1acf13078cedce5b
Marri Stephen <stephenmarri7@gmail.com>
2020-06-08 06:41:52 +0530

    problem 16 solved

---
+++ python/16.py
@@ -0,0 +1,14 @@
+muls = [2]
+
+for i in range(1,1000):
+    carry = 0
+    for x in range(len(muls)-1,-1,-1):
+        muls[x]=muls[x]+muls[x] + carry
+        if muls[x] >=10:
+            carry = muls[x]//10
+            muls[x]=muls[x]%10
+        else:
+            carry=0
+    if carry>0:
+        muls.insert(0,carry)
+print(sum(muls))
