6f71aab8a5f51466019b2aaaab2c40dc528b3187
Marri Stephen <stephenmarri7@gmail.com>
2020-06-23 08:33:19 +0530

    solved 31

--- python/31.py
+++ python/31.py
@@ -1,20 +1,20 @@
-sum=0
+    sum=0
 
 i,j,k,l,m,n,o=0,0,0,0,0,0,0
 
 while i<=200:
-    j, k, l, m, n, o = 0, 0, 0, 0, 0, 0
-    while i+j<=200:
-        k, l, m, n, o = 0, 0, 0, 0, 0
-        while i+j+k<=200:
-            l, m, n, o = 0, 0, 0, 0
-            while i+j+k+l<=200:
-                m, n, o = 0, 0, 0
-                while i+j+k+l+m<=200:
-                    n, o = 0, 0
-                    while i+j+k+l+m+n<=200:
-                        o = 0
-                        while i+j+k+l+m+n+o<=200:
+    j=i
+    while j<=200:
+        k=j
+        while k<=200:
+            l=k
+            while l<=200:
+                m=l
+                while m<=200:
+                    n=m
+                    while n<=200:
+                        o = n
+                        while o<=200:
                             sum+=1
                             o+=2
                         n+=5
