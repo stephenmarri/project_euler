e72815725569f8ad5bbf77e1ba5a4eaf24ed6efa
Marri Stephen <stephenmarri7@gmail.com>
2020-06-24 18:36:03 +0530

    first push

---
+++ site/data.js
@@ -0,0 +1,42 @@
+const data = {
+    item01:{
+        id:1,
+        question:`If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        Find the sum of all the multiples of 3 or 5 below 1000.`,
+        code:{
+            count:2,
+            python:"python/11.py",
+            javascript:"javascript/1.js"
+        },
+        answer:"123",
+        info:{
+            question_link:"https://projecteuler.net/problem=1",            
+        }
+    },
+    item02:{
+        id:2,
+        question:"If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.",
+        code:{
+            count:2,
+            python:"/python/12.py",
+            javascript:"javascript/1.js"
+        },
+        answer:"123",
+        info:{
+            question_link:"https://projecteuler.net/problem=1",            
+        }
+    },
+    item03:{
+        id:3,
+        question:"If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.",
+        code:{
+            count:2,
+            python:"file:///C:/StephenMarri/web_dev/project_euler(19)/python/13.py",
+            javascript:"javascript/1.js"
+        },
+        answer:"123",
+        info:{
+            question_link:"https://projecteuler.net/problem=1",            
+        }
+    }
+}
--- site/script.js
+++ site/script.js
@@ -8,6 +8,12 @@ modal.addEventListener('click',modalHandler)
 menuButton.addEventListener('click',menuHandler)
 problem_button.addEventListener('click',menuHandler)
 
+//content
+let questionHtml=document.querySelector('#q_text')
+let answerHtml=document.querySelector('#ans_text')
+let codeHtml=document.querySelector('#code_text')
+let infoHtml=document.querySelector('#info_text')
+
 function menuHandler(){
     modal.classList.toggle('active')
 }
@@ -20,6 +26,29 @@ function modalHandler(){
         index_tiles.forEach(x=>x.classList.remove('active'))
         prob.classList.add('active')
         section_header.textContent=String(event.target.textContent).replace(" -","")
+        populate(event.target.id)
     }
 }
 //##################################### modal
+
+async function populate(id){
+   const total= Object.keys(data).length
+   if(id < 0 || id > total) return
+
+    let question = data[`item${id}`]["question"]
+    let code = data[`item${id}`]["code"]
+    let answer = data[`item${id}`]["answer"]
+    let info = data[`item${id}`]["info"]
+
+    questionHtml.textContent=question
+    codeHtml.textContent=code
+    answerHtml.textContent=answer
+    infoHtml.textContent=info
+     
+
+    fetch(data[`item${id}`]["code"]["python"], {mode: "no-cors"}).then(res => res.text()).then(text => console.log(text))
+
+    
+    
+}
+
